cfa47c13ae4c243c2047ec82ba9782a59bab647f
Aidan Rand <aidan.rand13@gmail.com>
2023-07-18 15:27:56 +0100

    Parsed json for players and scoring info

--- main.py
+++ main.py
@@ -28,11 +28,24 @@ away_team_play_by_play = json_data.get('team_away')
 away_batters = json_data.get('away_batters')
 boxscore = json_data.get('boxscore')
 boxscore_teams = boxscore.get('teams')
-print(boxscore_teams)
-print(boxscore.keys(),'\n')
+away_players_dict = boxscore_teams.get('away').get('players')
+away_players = {}
+for player_info in away_players_dict.values():
+    player_id = player_info.get('person').get('id')
+    player_name = player_info.get('person').get('fullName')
+    away_players[player_name] = player_id
 
-print(json_data.keys(),'\n')
-#yes this is it
+home_players_dict = boxscore_teams.get('home').get('players')
+home_players = {}
+for player_info in home_players_dict.values():
+    player_id = player_info.get('person').get('id')
+    player_name = player_info.get('person').get('fullName')
+    home_players[player_name] = player_id
+
+print(away_players)
+print(home_players)
+
+#To Do:
 #print(json_data.get('team_home'),'\n')
 
 
@@ -40,20 +53,18 @@ away_team_runs_by_inning = ['Away: ']
 away_scoring_innings = []
 home_team_runs_by_inning = ['Home: ']
 home_scoring_innings = []
-print(scoreboard_by_inning)
-# for each in scoreboard_by_inning:
-#     runs = each.get('home')
-#         #.get('runs')
-#     print(runs)
-#     home_team_runs_by_inning.append(runs)
-    # if runs > 0:
-    #     home_scoring_innings.append(each.get('num'))
-    # runs = each.get('away').get('runs')
-    # away_team_runs_by_inning.append(runs)
-    # if runs > 0:
-    #     away_scoring_innings.append(each.get('num'))
 
-print(home_team_runs_by_inning)
-print(home_scoring_innings)
-print(away_team_runs_by_inning)
-print(away_scoring_innings)
+for each in scoreboard_by_inning:
+    runs = each.get('home').get('runs')
+    # no runs if bottom of ninth isn't played
+    if runs is None:
+        home_team_runs_by_inning.append('X')
+    else:
+        home_team_runs_by_inning.append(runs)
+        if runs > 0:
+            home_scoring_innings.append(each.get('num'))
+
+    runs = each.get('away').get('runs')
+    away_team_runs_by_inning.append(runs)
+    if runs > 0:
+        away_scoring_innings.append(each.get('num'))
