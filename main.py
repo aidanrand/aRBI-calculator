63ba46194e67ac477a1a8a4ea156e4e4938fc3dc
Aidan Rand <aidan.rand13@gmail.com>
2023-08-10 16:51:25 -0400

    Parsed data for plays and their descriptions during innings where the batting team scores

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ scoreboard_by_inning = scoreboard.get('linescore').get('innings')
 home_team_data = json_data.get('home_team_data')
 home_team = home_team_data.get('name')
 home_abbreviation = home_team_data.get('abbreviation')
-home_team_play_by_play = json_data.get('team_home')
-
+#data for team is listed when they are pitching, not when they are batting
+home_team_play_by_play = json_data.get('team_away')
 
 away_team_data = json_data.get('away_team_data')
 away_team = away_team_data.get('name')
 away_abbreviation = away_team_data.get('abbreviation')
-away_team_play_by_play = json_data.get('team_away')
+away_team_play_by_play = json_data.get('team_home')
 
 away_batters = json_data.get('away_batters')
 boxscore = json_data.get('boxscore')
@@ -51,20 +51,57 @@ print(home_players)
 
 away_team_runs_by_inning = ['Away: ']
 away_scoring_innings = []
+away_scoring_inning_plays = {}
+away_team_runs = {}
+
 home_team_runs_by_inning = ['Home: ']
 home_scoring_innings = []
+home_scoring_inning_plays = {}
+home_team_runs = {}
 
-for each in scoreboard_by_inning:
-    runs = each.get('home').get('runs')
+for inning in scoreboard_by_inning:
+    runs = inning.get('home').get('runs')
+    inningnum = inning.get('num')
     # no runs if bottom of ninth isn't played
     if runs is None:
         home_team_runs_by_inning.append('X')
     else:
         home_team_runs_by_inning.append(runs)
         if runs > 0:
-            home_scoring_innings.append(each.get('num'))
+            home_scoring_innings.append(inningnum)
+            home_scoring_inning_plays[inningnum] = []
+            home_team_runs[inningnum] = []
 
-    runs = each.get('away').get('runs')
+    runs = inning.get('away').get('runs')
     away_team_runs_by_inning.append(runs)
     if runs > 0:
-        away_scoring_innings.append(each.get('num'))
+        away_scoring_innings.append(inningnum)
+        away_scoring_inning_plays[inningnum] = []
+        away_team_runs[inningnum] = []
+
+print(home_scoring_inning_plays)
+print(away_scoring_inning_plays)
+for play in home_team_play_by_play:
+    inning = play.get('inning')
+    if inning in home_scoring_innings:
+        play_description = [play.get('batter_name'), play.get('des')]
+        print(play_description)
+        if len(home_scoring_inning_plays[inning]) == 0:
+            home_scoring_inning_plays[inning].append(play_description)
+        elif len(home_scoring_inning_plays[inning]) > 0 and home_scoring_inning_plays[inning][-1] != play_description:
+            home_scoring_inning_plays[inning].append(play_description)
+
+for play in away_team_play_by_play:
+    inning = play.get('inning')
+    if inning in away_scoring_innings:
+        play_description = [play.get('batter_name'), play.get('des')]
+        if len(away_scoring_inning_plays[inning]) == 0:
+            away_scoring_inning_plays[inning].append(play_description)
+        elif len(away_scoring_inning_plays[inning]) > 0 and away_scoring_inning_plays[-1] != play_description:
+            away_scoring_inning_plays[inning].append(play_description)
+
+print(home_scoring_inning_plays)
+
+print(away_scoring_inning_plays)
+
+# print(home_team_play_by_play)
